d6ee0f879e34db77157f82ea8cc9e79457d5e87f
jeffrey <vkefhdl1@gmail.com>
2024-01-01 15:57:54 +0900

    compress ingestion by token f1 score

--- ingest.py
+++ ingest.py
@@ -3,14 +3,16 @@ Ingest rem sqlite3 database to RAGchain
 We use local json linker, pickle DB, and Chroma Vector DB.
 Plus, I'll use openai embedding for ingest, but you can use any embedding you want.
 """
+import itertools
 import os
 from datetime import datetime, timedelta
 
 import chromadb
 import click
 from RAGchain.DB import PickleDB
-from RAGchain.pipeline import BasicIngestPipeline
+from RAGchain.benchmark.answer.metrics import KF1
 from RAGchain.preprocess.loader.rem_loader import RemLoader
+from RAGchain.preprocess.text_splitter import TokenSplitter
 from RAGchain.retrieval import VectorDBRetrieval, HybridRetrieval, BM25Retrieval
 from RAGchain.utils.embed import EmbeddingFactory
 from RAGchain.utils.vectorstore import ChromaSlim
@@ -46,8 +48,31 @@ def main(db_path, ingest_minutes):
     retrieval = HybridRetrieval([VectorDBRetrieval(vectordb), BM25Retrieval(BM25_PATH)], [0.7, 0.3],
                                 method='cc')
 
-    pipeline = BasicIngestPipeline(file_loader=loader, db=db, retrieval=retrieval, ignore_existed_file=False)
-    pipeline.run()
+    documents = loader.load()
+    ingest_documents = []
+    # keep a document that has no duplicate
+    for i, document in enumerate(documents):
+        if i == 0:
+            ingest_documents.append(document)
+            continue
+        # calculate similarity with a previous document using token f1 score
+        kf1 = KF1()
+        score = kf1._token_f1_score(document.page_content, documents[i - 1].page_content)
+        if score < 0.9:
+            ingest_documents.append(document)
+
+    print(f"Total {len(documents)} documents, {len(ingest_documents)} documents will be ingested.")
+
+    splitter = TokenSplitter(chunk_size=1024, chunk_overlap=128)
+    passages = splitter.split_documents(ingest_documents)
+
+    print(f"Total {len(passages)} passages will be ingested.")
+
+    # ingest to db and retrieval
+    db.create_or_load()
+    db.save(list(itertools.chain.from_iterable(passages)))
+
+    retrieval.ingest(list(itertools.chain.from_iterable(passages)))
 
 
 if __name__ == '__main__':
