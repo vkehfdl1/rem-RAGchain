17bf8c9ca8d240c8ed9cebf8376c954cdfb2ec35
jeffrey <vkefhdl1@gmail.com>
2024-01-01 16:26:37 +0900

    add WeightedTimeReranker

--- run_llm.py
+++ run_llm.py
@@ -1,13 +1,19 @@
+from operator import itemgetter
+from typing import List
+
 import chromadb
 import click
-from RAGchain.pipeline import BasicRunPipeline
-from RAGchain.retrieval import VectorDBRetrieval
+from RAGchain.reranker.time import WeightedTimeReranker
+from RAGchain.retrieval import VectorDBRetrieval, HybridRetrieval, BM25Retrieval
 from RAGchain.schema import Passage, RAGchainChatPromptTemplate
 from RAGchain.utils.embed import EmbeddingFactory
 from RAGchain.utils.vectorstore import ChromaSlim
 from langchain_community.chat_models import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 
 CHROMA_DB_PATH = 'Chroma/'
+BM25_PATH = 'DB/bm25.pkl'
 PROMPT = RAGchainChatPromptTemplate.from_messages([
     ("user", "Please answer {question}"),
     ("system", "--------------- Below is the text that\'s been on screen recently. --------------"),
@@ -17,21 +23,45 @@ PROMPT = RAGchainChatPromptTemplate.from_messages([
                "{question}"),
 ])
 
+vectordb = ChromaSlim(
+    client=chromadb.PersistentClient(path=CHROMA_DB_PATH),
+    collection_name='rem',
+    embedding_function=EmbeddingFactory('openai', device_type='mps').get()
+)
+retrieval = HybridRetrieval([VectorDBRetrieval(vectordb), BM25Retrieval(BM25_PATH)], [0.7, 0.3],
+                            method='cc')
+time_reranker = WeightedTimeReranker()
+
 
 @click.command()
 @click.option('--query', help='Query you want to ask to LLM.')
 def main(query):
     print(f"Question : {query}")
 
-    vectordb = ChromaSlim(
-        client=chromadb.PersistentClient(path=CHROMA_DB_PATH),
-        collection_name='rem',
-        embedding_function=EmbeddingFactory('openai', device_type='mps').get()
+    runnable = RunnablePassthrough.assign(
+        passages=itemgetter("question") | RunnableLambda(
+            lambda question: retrieve(question)),
+        question=itemgetter("question"),
+    ) | RunnablePassthrough.assign(
+        answer={
+                   "question": itemgetter("question"),
+                   "passages": itemgetter("passages") | RunnableLambda(
+                       lambda passages: Passage.make_prompts(passages),
+                   ),
+               } | PROMPT | ChatOpenAI() | StrOutputParser()
     )
-    pipeline = BasicRunPipeline(VectorDBRetrieval(vectordb), ChatOpenAI(), prompt=PROMPT)
-    answers, passages, scores = pipeline.get_passages_and_run([query])
-    print(f"Answer : {answers[0]}")
-    print(f"Passages : {Passage.make_prompts(passages[0])}")
+    answer = runnable.invoke({"question": query})
+
+    print(f"Answer : {answer['answer']}")
+    print(f"Passages : {Passage.make_prompts(answer['passages'])}")
+
+
+def retrieve(query: str, top_k: int = 4) -> List[Passage]:
+    ids, scores = retrieval.retrieve_id_with_scores(query, top_k=100)
+    passages = retrieval.fetch_data(ids)
+    reranked_passages_and_scores = time_reranker.rerank(passages, scores)
+    reranked_passages = [passage for passage, score in reranked_passages_and_scores]
+    return reranked_passages[:top_k]
 
 
 if __name__ == '__main__':
